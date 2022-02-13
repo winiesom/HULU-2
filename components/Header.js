d5bff9a5bf726fb2a036e2d2dd5dddcd352d6075
mmaesom <winifred.esomonu@stetis.com>
2022-02-13 13:55:38 +0100

    fixes

--- components/Header.js
+++ components/Header.js
@@ -23,6 +23,7 @@ const Header = () => {
       <Image
         className="object-contain"
         src="https://links.papareact.com/ua6"
+        alt="HULU"
         width={200}
         height={100}
       />
